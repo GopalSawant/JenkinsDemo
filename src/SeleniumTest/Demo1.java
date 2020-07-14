db9a70ca2ceb56ac8295e9b2c359b866595481f1
GopalSawant <ggopalsawant@gmail.com>
2020-07-14 11:07:35 +0530

    space removal

--- src/SeleniumTest/Demo1.java
+++ src/SeleniumTest/Demo1.java
@@ -11,9 +11,7 @@ public class Demo1 {
 
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Vaishali Sawant\\Desktop\\Eclipse\\Resources\\chromedriver83.exe");
 		WebDriver driver=new ChromeDriver();
-		driver.manage().window().maximize();
-
-		
+		driver.manage().window().maximize();		
 		driver.get("https://facebook.com");
 		driver.findElement(By.xpath("//input[@type='email']")).sendKeys("email");
 		driver.findElement(By.cssSelector("input[name='pass']")).sendKeys("pass");
